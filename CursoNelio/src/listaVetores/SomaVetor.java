86688008c29960d410df1f053924b617a2401f46
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-10-19 17:51:07 -0300

    Curso Nelio| Soma Vetor

--- CursoNelio/src/listaVetores/SomaVetor.java
+++ CursoNelio/src/listaVetores/SomaVetor.java
@@ -3,30 +3,33 @@ package listaVetores;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Negativos {
+public class SomaVetor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		double soma = 0;
 		
 		System.out.print("Quantos numeros você vai digitar? ");
 		int tamVetor= sc.nextInt();
-		int[] numeros = new int[tamVetor];
+		double[] numeros = new double[tamVetor];
 		
 		for (int i=0; i<numeros.length; i++) {
 			System.out.print("Digite um número: ");
-			numeros[i]= sc.nextInt();
+			double numero = sc.nextDouble();
+			numeros[i]= numero;
+			soma+=numero;
 		}
 		
-		System.out.println("NUMEROS NEGATIVOS:");
+		System.out.print("\nVALORES =");
 		for (int i=0; i<numeros.length; i++) {
-			if (numeros[i] <0)
-				System.out.println(numeros[i]);
+			System.out.printf(" %.1f", numeros[i]);
 		}
+		System.out.printf("\nSOMA = %.2f%n", soma);
+		System.out.printf("MEDIA = %.2f%n", soma/tamVetor);
 		
-		sc.close();
 	}
 
 }
